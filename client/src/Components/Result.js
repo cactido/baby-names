7c73d50fdddefbca5c66dca0b759574818b36552
Travis Russell <mtrussell@gmail.com>
2021-09-28 21:25:39 -0600

    working on Result logic

--- client/src/Components/Result.js
+++ client/src/Components/Result.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 const Result = () => {
-    const ALL_USERS = gql`
-        query {
+    /*
+    const GET_ALL_USERS = gql`
+        query getAllUsers {
             getAllUsers {
                 id
                 display_name
@@ -15,10 +16,35 @@ const Result = () => {
             }
         }
     `;
+    */
+
+    const GET_USER = gql`
+                query getUser($userId: ID!) {
+                    getUser(id: $userId) {
+                    provided_names {
+                        name
+                    }
+                    selected_names {
+                        name
+                    }
+                }
+            }
+        `;
+    const { data: user1Data, error: user1Error } = useQuery(GET_USER, { variables: { userId: '6150c05b7afa685d78a9d919' } });
+    const { data: user2Data, error: user2Error } = useQuery(GET_USER, { variables: { userId: '6153b9c8d58bd6ec1fe2340b' } });
+     
+    if (user1Error && console.log(user1Error));
+    if (user2Error && console.log(user2Error));
     
-    const { loading, error, data } = useQuery(ALL_USERS);
-    if (!loading) { console.log(data.getAllUsers[0]); }
-    if (error) { console.log(error); }
+    let user1Names = [];
+    let user2Names = [];
+
+    if (user1Data) user1Names = user1Data.getUser.provided_names;
+    if (user2Data) user2Names = user2Data.getUser.provided_names;
+
+    console.log('user1Names: ',user1Names);
+    console.log('user2Names: ',user2Names);
+
     return (
         <div>
 
