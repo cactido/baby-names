87a0aaefda230ab2702ab7c6d4dd7a3247f6747b
Ryely <ryely.moore@outlook.com>
2021-09-30 18:10:45 -0600

    added some styling to the List page and added additional navigation links

--- client/src/components/Header/Navigation.jsx
+++ client/src/components/Header/Navigation.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Nav, NavLink } from 'reactstrap';
+import { Nav, NavLink, Row, Col } from 'reactstrap';
 import Auth from '../../utils/auth';
 
 const Navigation = () => {
@@ -7,7 +7,25 @@ const Navigation = () => {
     const [pageState, setPageState] = useState({logged: false});
 
     const handleClick = (event) => {
-      Auth.logout();
+      switch (event.target.name) {
+        case 'logout':
+          Auth.logout();
+          break;
+        case 'Home':
+          window.location.assign(`/${event.target.name}`);
+          break;
+        case 'Entry':
+          window.location.assign(`/${event.target.name}`);
+          break;
+        case 'Compare':
+          window.location.assign(`/${event.target.name}`);
+          break;
+        case 'List':
+          window.location.assign(`/${event.target.name}`);
+          break;
+        
+      }
+      
     }
 
     useEffect(() => {
@@ -20,10 +38,38 @@ const Navigation = () => {
     return(
         <Nav className="d-flex flex-row-reverse">
           {pageState.logged && (
-            <NavLink  onClick={handleClick}
+            <Row>
+              <Col>
+              <NavLink name='Home' onClick={handleClick}
+            style={{color: 'lightpink', cursor: 'pointer'}} >
+           Home
+           </NavLink>
+           </Col>
+              <Col>
+              <NavLink name='List' onClick={handleClick}
+            style={{color: 'lightpink', cursor: 'pointer'}} >
+           Names
+           </NavLink>
+           </Col>
+              <Col>
+              <NavLink name='Compare' onClick={handleClick}
+            style={{color: 'lightpink', cursor: 'pointer'}} >
+           Rate
+           </NavLink>
+           </Col>
+              <Col>
+              <NavLink name='Entry' onClick={handleClick}
+            style={{color: 'lightpink', cursor: 'pointer'}} >
+           Add
+           </NavLink>
+           </Col>
+              <Col>
+              <NavLink name='logout'  onClick={handleClick}
             style={{color: 'lightpink', cursor: 'pointer'}} >
            Logout
            </NavLink>
+           </Col>
+            </Row>
           )}
         </Nav>
     )
